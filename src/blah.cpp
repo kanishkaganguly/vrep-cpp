612f668c234934e9b1e35320057aff8803680172
Kanishka Ganguly <kganguly@umd.edu>
2018-05-15 13:34:34 -0400

    Fetch path data from server

--- src/blah.cpp
+++ src/blah.cpp
@@ -2,15 +2,17 @@
 // Created by kganguly on 5/1/18.
 //
 
+#include <vector>
 #include "blah.h"
 
 int main(int argc, char *argv[]) {
     int path_handle = -1;
     int dummy_handle = -1;
-    float *pos_eul;
-    float percent_path = 0.1;
-    int count_pos_eul = 6;
-    float *path_pos_eul;
+    float percent_of_path = 0.1f;
+
+    int count_floats_from_server;
+    float *floats_from_server;
+
 //    vrep_utils::launch_vrep();
 
     vrep_utils::setup_vrep_remote();
@@ -23,12 +25,25 @@ int main(int argc, char *argv[]) {
 
             while (simxGetConnectionId(vrep_utils::_clientID) != -1) {
                 simxCallScriptFunction(vrep_utils::_clientID, "Path", sim_scripttype_childscript, "fetchPath_function",
-                                       0, NULL,
-                                       1, &percent_path, 0, NULL, 0, NULL, NULL, NULL, &count_pos_eul, &path_pos_eul,
-                                       NULL, NULL, NULL, NULL, simx_opmode_blocking);
+                                       1, &path_handle,                   // input int count, input int data
+                                       1, &percent_of_path,               // input float count, input float data
+                                       0, NULL,                           // input string count, input string data
+                                       0, NULL,                           // input buffer size, input buffer data
+                                       NULL, NULL,                        // output int count, output int data
+                                       &count_floats_from_server,
+                                       &floats_from_server, // output float count, output float data
+                                       NULL, NULL,                        // output string count, output string data
+                                       NULL, NULL,                        // output buffer count, output buffer data
+                                       simx_opmode_blocking);
+                std::vector<float> vrep_vals(floats_from_server, floats_from_server + count_floats_from_server);
+                for (int i = 0; i < count_floats_from_server; ++i) {
+                    std::cout << vrep_vals[i] << " ";
+                }
+                std::cout << std::endl;
             }
         }
     } catch (char *msg) {
+        vrep_utils::stop_sim();
         std::cout << msg << std::endl;
     }
 }
